6ed9d6c4dfad7baced6dc7521af2041d3273b1f8
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-04-26 17:46:23 +0530

    route changed

--- Controllers/User.js
+++ Controllers/User.js
@@ -120,7 +120,7 @@ exports.forgotPassword = async (req, res) => {
       text:
         `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n` +
         `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-        `${req.headers.origin}/reset-password/${token}\n\n` +
+        `${req.headers.origin}/resetpass/${token}\n\n` +
         `If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
